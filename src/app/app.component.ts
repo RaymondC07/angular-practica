668e993c54fe1f9e97feb91402165bcf2e2782cc
Raymond Castellanos <app.raycastell@gmail.com>
2022-08-24 16:05:29 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html'
+  // template: `
+  //   <h3>Este método lo que hace es ignorar el app.component.html</h3>
+  //   <p>No quiero trabajarlo por éste lado</p>
+  // `
 })
 export class AppComponent {
-  title = 'bases';
+
 }
